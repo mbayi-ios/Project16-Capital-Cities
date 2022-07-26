9c1400bc2563a93f9eae0922d011bd23b9513477
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-07-26 10:10:49 +0300

    Initial Commit

---
+++ Project16-Capital-Cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16-Capital-Cities
+//
+//  Created by Amby on 26/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
